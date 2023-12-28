4d0f2a4dc0ecffa282160be9f35d440d207f814e
IlyaBabich1994 <ktljva@gmail.com>
2023-12-28 18:17:10 +0100

    init

---
+++ src/main/java/com/example/telegramfilter/TelegramFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegramfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegramfilter/TelegramFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegramfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
