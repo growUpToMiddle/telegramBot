7c7e1996cee22636c51e77b4590e279a3e09e8fb
IkitTheClaw <IkitTheClaw@gmail.com>
2024-01-15 20:49:23 +0300

    Chat Filter with .contains (inner storage for banwords)

--- src/main/java/com/example/telegramfilter/telegram/Bot.java
+++ src/main/java/com/example/telegramfilter/telegram/Bot.java
@@ -1,5 +1,10 @@
 package com.example.telegramfilter.telegram;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -12,14 +17,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+
 public class Bot extends TelegramLongPollingBot {
     private final List<String> banwords = new ArrayList<>();
-
-
+@Value("${bot.token}")//?
     private String BOT_TOKEN;
+@Value("${bot.name}")//?
     private String BOT_NAME;
 
     public Bot() {
+        BOT_NAME = "@Omnisiabot";
+        BOT_TOKEN = "6720727438:AAFmtwmS6tc23xJeUVQ-yp9zsauGLw0EFl0";
 
         banwords.add("horse");
         banwords.add("frog");
@@ -34,9 +42,6 @@ public class Bot extends TelegramLongPollingBot {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        BOT_TOKEN = properties.getProperty("bot.token");
-        BOT_NAME = properties.getProperty("bot.name");
     }
 
     @Override
@@ -53,29 +58,29 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-            Message inMess = update.getMessage(); // смотрим что написано в сообщении пользователя
+            Message inMess = update.getMessage();
 
-                    String chatId = inMess.getChatId().toString();
-                    String response = parseMessage(inMess.getText());
-                    SendMessage outMess = new SendMessage();
-                    outMess.setChatId(chatId);
-                    outMess.setText(response);
-                    try {
-                        if (!response.isBlank()){
-                            execute(outMess);
-                        }
-                    } catch (TelegramApiException e) {
-                        throw new RuntimeException(e);
-                    }
+            String chatId = inMess.getChatId().toString();
+            String response = parseMessage(inMess.getText());
+            SendMessage outMess = new SendMessage();
+            outMess.setChatId(chatId);
+            outMess.setText(response);
+            try {
+                if (!response.isBlank()) {
+                    execute(outMess);
+                }
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
             }
+        }
     }
 
     public String parseMessage(String textMsg) {
         String response = new String();
 
-        for (String banword:banwords) {
+        for (String banword : banwords) {
             if (textMsg.toLowerCase().contains(banword)) {
-                response = "Мы тут не любим -" + banwords + " поаккуратней со словами";
+                response = "Мы тут таких как \"" + banword.toUpperCase() + "\" не любим";
 
             }
         }
