bfaf9507389602c32d256bf54278a431b556caef
IkitTheClaw <IkitTheClaw@gmail.com>
2024-01-17 21:29:32 +0300

    fixed#3

--- src/main/java/com/example/telegramfilter/Bot.java
+++ src/main/java/com/example/telegramfilter/Bot.java
@@ -4,7 +4,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-<<<<<<< HEAD
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
@@ -18,16 +17,6 @@ public class Bot extends TelegramLongPollingBot {
 
     private final List<String> banwords = new ArrayList<>();
 
-=======
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Properties;
-
-public class Bot extends TelegramLongPollingBot {
-
->>>>>>> 30466b1f784af54791ce802f5f5d1138cc491fac
     protected static final String BOT_TOKEN;
     protected static String botName;
 
@@ -46,7 +35,6 @@ public class Bot extends TelegramLongPollingBot {
     public Bot(String BOT_NAME, String BOT_TOKEN) {
         super(BOT_TOKEN);
         botName = BOT_NAME;
-<<<<<<< HEAD
 
         banwords.add("horse");
         banwords.add("frog");
@@ -57,10 +45,6 @@ public class Bot extends TelegramLongPollingBot {
     }
 
 
-=======
-    }
-
->>>>>>> 30466b1f784af54791ce802f5f5d1138cc491fac
     @Override
     public String getBotUsername() {
         return botName;
@@ -75,7 +59,6 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         if (update.hasMessage() && update.getMessage().hasText()) {
-<<<<<<< HEAD
             Message inMess = update.getMessage();
 
             String chatId = inMess.getChatId().toString();
@@ -106,14 +89,6 @@ public class Bot extends TelegramLongPollingBot {
     }
 
 
-=======
-            Message inMess = update.getMessage(); // смотрим что написано в сообщении пользователя
-            String chatId = inMess.getChatId().toString(); // достаём id чата
-            SendMessage outMess = new SendMessage(); // создаём наш ответ пользователю
-        }
-    }
-
->>>>>>> 30466b1f784af54791ce802f5f5d1138cc491fac
     @Override
     public void onUpdatesReceived(List<Update> updates) {
         super.onUpdatesReceived(updates);
